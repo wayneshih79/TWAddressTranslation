b3e54d97d4f7024306fd56e45c2cc6dee2db7d26
Shih <shihweiyao@gmail.com>
2018-11-03 22:59:45 +0800

    123

--- app/src/App.js
+++ app/src/App.js
@@ -1,43 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Home from './Home';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import GroupList from './GroupList';
+import GroupEdit from './GroupEdit';
 
 class App extends Component {
-  state = {
-    isLoading: true,
-    groups: []
-  };
-
-  async componentDidMount() {
-    const response = await fetch('/api/groups');
-    const body = await response.json();
-    this.setState({ groups: body, isLoading: false });
-  }
-
-  render() {
-    const {groups, isLoading} = this.state;
-
-    if (isLoading) {
-      return <p>Loading...</p>;
+    render() {
+        return (
+            <Router>
+              <Switch>
+                <Route path='/' exact={true} component={Home}/>
+                <Route path='/groups' exact={true} component={GroupList}/>
+                <Route path='/groups/:id' component={GroupEdit}/>
+              </Switch>
+            </Router>
+        )
     }
-
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <div className="App-intro">
-          <h2>JUG List</h2>
-          {groups.map(group =>
-            <div key={group.id}>
-              {group.name}
-            </div>
-          )}
-        </div>
-      </div>
-    );
-  }
 }
 
 export default App;
